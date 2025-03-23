fa7f71a6c75bfa2f42e4374f50afdc4dde095929
bryce <a0955361101@gmail.com>
2025-03-23 10:48:38 +0800

    change url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/poop-counter/",
+    base: "/poop-tracker/",
     plugins: [vue()],
     server: {
         port: 3000,
